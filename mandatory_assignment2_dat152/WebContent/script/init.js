db041ef536ef02c50ea3ed8e6cb2b2b2250bb7d1
magnusrambech <magnusrambech@msn.com>
2018-09-26 14:24:21 +0200

    hacky get all

--- mandatory_assignment2_dat152/WebContent/script/init.js
+++ mandatory_assignment2_dat152/WebContent/script/init.js
@@ -1,53 +1,17 @@
 "use strict"
 var memberIdElm = null
 
-function init() {
-	
-	
-	//  IKKE AJAX 
-	//     |
-	//     v
-	//
-//    const ui = new UIHandler()
-//    document.getElementById('memberlist').appendChild(ui.memberList);
-//
-//    ui.addMember({
-//        'memberId': 1,
-//        'firstname': 'Ole',
-//        'lastname':'Olsen' ,
-//        'address': 'Olsenbakken',
-//        'phone': '91826453'
-//    })
-//
-//    ui.addMember({
-//        'memberId': 2,
-//        'firstname': 'Anne',
-//        'lastname':'Annesen',
-//        'address': 'Annesvingen',
-//        'phone': '87655458'
-//    })
-//
-//    ui.editMember({
-//        'memberId': 1,
-//        'firstname': 'Ole',
-//        'lastname':'Olsen' ,
-//        'address': 'Olsenbakken',
-//        'phone': '11223344'
-//    })
-//
-//    ui.deleteMember(2);
-
-
-}
 
 function load() {
   	document.getElementById("getMember").addEventListener("click",getMember,false)
   	memberIdElm = document.getElementById("memberId")
+  	document.getElementById('addMemberBtn').addEventListener("click",addMember,false)
     }
 
 function viewResult(m){
 	document.getElementById("memberInfo").textContent=m;
 }
+
 function getMember(e){
 	let url = config.servicesPath + "/member"
     const ajax = new AJAXConnection(url)
@@ -56,14 +20,39 @@ function getMember(e){
 
 }
 
+function addMember(e){
+	let json={};
+
+	json.firstname = document.getElementById("firstName").value;
+	json.lastname = document.getElementById("lastName").value;
+	json.phone = document.getElementById("phone").value;
+	json.address = document.getElementById("address").value;
+	
+	console.log(json)
+	let url = config.servicesPath + "/member";
+	const ajax = new AJAXConnection(url);	
+	ajax.post(url, json);
+	
+	
+	
+}
+
 function initAjax(){
-	const ui = new UIHandler();
+	let reachedEnd = false;
+	let curr = 1;
 	document.getElementById('memberlist').appendChild(ui.memberList);
-	let url = config.servicesPath;
+	let url = config.servicesPath + "/member";
 	const ajax = new AJAXConnection(url);	
 	
 	// TODO: hent ut alle medlemmer ved bruk av 'ajax' (AJAXConnection) og vis de på siden ved bruk av 'ui' (UIHandler)
 	
+	for(let i=1; i<100; i++){
+		console.log("CURR: "+ i)
+		ajax.getAll([i]);
+		
+	}
+		
+	
 }
 window.addEventListener("DOMContentLoaded",load,true)
-document.addEventListener('DOMContentLoaded',initAjax,true)
+window.addEventListener('DOMContentLoaded',initAjax,true)
